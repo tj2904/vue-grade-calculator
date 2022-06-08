d4a3dddce5950ab982a5e34caa1ff956e5534e2c
Tim <tj2904@users.noreply.github.com>
2022-06-08 10:54:32 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
 module.exports = {
   publicPath: '/vue-grade-calculator/'
 }
