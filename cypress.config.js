58ed1f6f60eda2cae72ca72315b4b455916755a6
Tim <tj2904@users.noreply.github.com>
2022-06-25 18:21:41 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: secrets.CYPRESS_RECORD_KEY
-    setupNodeEvents(on, config) {
+     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: "http://localhost:8080/vue-grade-calculator/"
