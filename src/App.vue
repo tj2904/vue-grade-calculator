ddbf9bf196e1282f3626e117411ec57f09ba80c8
Tim <tj2904@users.noreply.github.com>
2022-07-14 14:07:14 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -41,8 +41,8 @@
 		</section>
 
 		<footer>
-			<a href="https://github.com/tj2904/vue-grade-calculator" aria-label="Link to github"><i
-					class="fa fa-github"></i></a>
+		<!--	<a href="https://github.com/tj2904/vue-grade-calculator" aria-label="Link to github"><i
+					class="fa fa-github"></i></a> -->
 		</footer>
 
 	</main>
